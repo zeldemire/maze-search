4207be55b004573c6690033d86d9fb5eeb027556
Zach Eldemire <zeldemire@gmail.com>
2017-10-02 01:38:35 -0400

    Changed the style (tabs)

--- src/mazesearch/cse/miamioh/edu/SearchMain.java
+++ src/mazesearch/cse/miamioh/edu/SearchMain.java
@@ -3,9 +3,17 @@ package mazesearch.cse.miamioh.edu;
 
 public class SearchMain {
 
-	  public static void main(String args[]) {
-		  
-	  }
-
+	public static void main(String args[]) {
+		Maze maze = new Maze("D:\\Documents\\School\\AI\\maze-search\\sample-input\\maze1.txt");
+		SearchStrategy strategy = new SearchStrategy(maze) {
+			@Override
+			public void solve() {
+				BFS bfs = new BFS(maze.getStart(), maze.getGoal());
+				DFS dfs = new DFS(maze.getStart(), maze.getGoal());
+				A_STAR a_star = new A_STAR(maze.getStart(), maze.getGoal());
+			}
+		};
 	}
+
+}
 
