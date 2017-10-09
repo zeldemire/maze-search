b23cf2d1aa171122b5e84037d6ca76ee3e3fdd6a
Zach Eldemire <zeldemire@gmail.com>
2017-10-08 23:35:57 -0400

    Added the components to start the program

--- src/mazesearch/cse/miamioh/edu/SearchMain.java
+++ src/mazesearch/cse/miamioh/edu/SearchMain.java
@@ -4,15 +4,34 @@ package mazesearch.cse.miamioh.edu;
 public class SearchMain {
 
 	public static void main(String args[]) {
-		Maze maze = new Maze("D:\\Documents\\School\\AI\\maze-search\\sample-input\\maze1.txt");
-		SearchStrategy strategy = new SearchStrategy(maze) {
-			@Override
-			public void solve() {
-				BFS bfs = new BFS(maze.getStart(), maze.getGoal());
-				DFS dfs = new DFS(maze.getStart(), maze.getGoal());
-				A_STAR a_star = new A_STAR(maze.getStart(), maze.getGoal());
-			}
-		};
+//		Maze maze = new Maze("E:\\Documents\\School\\AI\\maze-search\\sample-input\\maze1.txt");
+//		Maze maze = new Maze("E:\\Documents\\School\\AI\\maze-search\\sample-input\\maze2.txt");
+		Maze maze = new Maze("E:\\Documents\\School\\AI\\maze-search\\sample-input\\maze3.txt");
+		BFS bfs = new BFS(maze);
+		DFS dfs = new DFS(maze);
+		A_STAR a_star = new A_STAR(maze);
+
+		a_star.solve();
+		System.out.println("A_STAR");
+		System.out.print("Actions taken: ");
+		System.out.println(a_star.getPathFromStartToGoal().size());
+		System.out.print("Explored nodes: ");
+		System.out.println(a_star.getExploredSquares().size());
+		System.out.println("A_STAR cost " + a_star.getPathFromStartToGoal().size());
+
+		dfs.solve();
+		System.out.println("DFS");
+		System.out.print("Actions taken: ");
+		System.out.println(dfs.getPathFromStartToGoal().size());
+		System.out.print("Explored nodes: ");
+		System.out.println(dfs.getExploredSquares().size());
+		bfs.solve();
+
+		System.out.println("BFS");
+		System.out.print("Actions taken: ");
+		System.out.println(bfs.getPathFromStartToGoal().size());
+		System.out.print("Explored nodes: ");
+		System.out.println(bfs.getExploredSquares().size());
 	}
 
 }
